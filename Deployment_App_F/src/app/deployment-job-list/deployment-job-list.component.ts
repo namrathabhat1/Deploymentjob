0d91def5c2a95f3cc1f466eb2ee5125099f80c27
Namratha Bhat <52617879+namrathabhat1@users.noreply.github.com>
2025-08-26 12:11:58 +0530

    Update deployment-job-list.component.ts
    component to force ui update

--- Deployment_App_F/src/app/deployment-job-list/deployment-job-list.component.ts
+++ Deployment_App_F/src/app/deployment-job-list/deployment-job-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { JobService, DeploymentJob } from '../job.service';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { Router } from "@angular/router";
+import { JobService, DeploymentJob } from "../job.service";
 
 @Component({
-  selector: 'app-deployment-job-list',
-  templateUrl: './deployment-job-list.component.html',
-  styleUrls: ['./deployment-job-list.component.css'],
+  selector: "app-deployment-job-list",
+  templateUrl: "./deployment-job-list.component.html",
+  styleUrls: ["./deployment-job-list.component.css"],
 })
 export class DeploymentJobListComponent implements OnInit {
   jobs: DeploymentJob[] = [];
 
-  constructor(private jobService: JobService, private router: Router) {}
+  constructor(
+    private jobService: JobService,
+    private router: Router,
+    private cdr: ChangeDetectorRef  //built in reference object from Angular
+ ) {}
 
   ngOnInit() {
     this.loadJobs();
@@ -18,19 +22,23 @@ export class DeploymentJobListComponent implements OnInit {
 
   loadJobs() {
     this.jobService.getJobs().subscribe({
-      next: (data: DeploymentJob[]) => (this.jobs = data),
-      error: (err: any) => console.error('Error loading jobs:', err),
+      next: (data: DeploymentJob[]) => {
+        this.jobs = data;
+        console.log("API data, list of jobs", this.jobs);
+        this.cdr.detectChanges(); //force UI update
+      },
+      error: (err: any) => console.error("Error loading jobs:", err),
     });
   }
 
   deleteJob(id: number) {
     this.jobService.deleteJob(id).subscribe({
       next: () => this.loadJobs(),
-      error: (err: any) => console.error('Error deleting job:', err),
+      error: (err: any) => console.error("Error deleting job:", err),
     });
   }
 
   editJob(id: number) {
-    this.router.navigate(['/create', id]);
+    this.router.navigate(["/create", id]);
   }
 }
